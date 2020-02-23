4669f034b313a4e1084cd8c65815c2094877f92b
Korejap <Korejap@users.noreply.github.com>
2020-02-23 16:40:25 +0100

    szyfro zrobiony system podpowiedzi ale trzeba poprawic na to by podawac pszczegolne cyfry

--- src/Zajecia6/SzyfrolamaczBezMetod.java
+++ src/Zajecia6/SzyfrolamaczBezMetod.java
@@ -15,9 +15,14 @@ public class SzyfrolamaczBezMetod {
         String posredniaLiczba = "";
         int tablicaLiczba = 0;
 
+int podpowiedz = 0;
+
+int liczbaPodpowiedzi = 0;
+int losowaDoPodpowiedzi = 0;
+
 //petla gry
 
-        while (zycie > 0) {
+        while (zycie > -1) {
 // ustawSzyfr;
             if (zycie == 6) {
                 posredniaLiczba = "";
@@ -25,53 +30,82 @@ public class SzyfrolamaczBezMetod {
                 for (int i = 0; i < tablica.length; i++) {
                     tablica[i] = random.nextInt(10);
                 }
-            for (int i = 0; i < tablica.length; i++) {
+                for (int i = 0; i < tablica.length; i++) {
 
-                posredniaLiczba += tablica[i];
+                    posredniaLiczba += tablica[i];
+                }
+                tablicaLiczba = Integer.valueOf(posredniaLiczba);
+                zycie--;
+                losowaDoPodpowiedzi = 0;
+                liczbaPodpowiedzi = 0;
             }
-            tablicaLiczba = Integer.valueOf(posredniaLiczba);
-        }
 
 
 //sprawdzanie
             if (zycie > 0) {
                 System.out.println("Podaj czterocyfrowy szyfr");
                 int odpowiedz = scanner.nextInt();
-                    if (tablicaLiczba > odpowiedz) {
-                        zycie--;
-                        System.out.println("Podana liczba jest mniejsza od szyfru.");
-                        System.out.println("Tracisz jedno życie.. Pozostało:" + zycie);
-                    } else if (tablicaLiczba < odpowiedz) {
-                        zycie--;
-                        System.out.println("Podana liczba jest większa od szyfru.");
-                        System.out.println("Tracisz jedno życie. Pozostało:" + zycie);
-                    } else if (tablicaLiczba == odpowiedz) {
-                        System.out.println("Gratulacje! Podałeś prawidłową odpowiedź!");
-                        System.out.println("Czy chcesz grać dalej? Y - tak | N - nie");
-                        String coDalej = scanner.nextLine().toUpperCase();
-                        switch (coDalej) {
-                            case "Y":
-                                zycie = 6;
+                if (tablicaLiczba > odpowiedz) {
+                    zycie--;
+                    System.out.println("Podana liczba jest mniejsza od szyfru.");
+                    System.out.println("Tracisz jedno życie.. Pozostało:" + zycie);
 
-                            case "N":
-                                System.exit(0);
-                                break;
+                    //podpowiedz
+                    System.out.println("Czy chcesz podpowiedź?");
+                    podpowiedz = scanner.nextInt();
 
-                        }
+
+                    if(podpowiedz == 1) {
+                        liczbaPodpowiedzi--;
+                        losowaDoPodpowiedzi = random.nextInt(4);
+                        System.out.println("Cyfrą " + (losowaDoPodpowiedzi + 1) + "od lewej strony jest: " + tablica[losowaDoPodpowiedzi]);
                     }
+
+                } else if (tablicaLiczba < odpowiedz) {
+                    zycie--;
+                    System.out.println("Podana liczba jest większa od szyfru.");
+                    System.out.println("Tracisz jedno życie. Pozostało:" + zycie);
+
+                    //podpowiedz
+                    System.out.println("Czy chcesz podpowiedź? Wcisnij 1 jesli tak.");
+                    podpowiedz = scanner.nextInt();
+                    switch (podpowiedz){
+                        case 1:
+                        liczbaPodpowiedzi--;
+                        losowaDoPodpowiedzi = random.nextInt(4);
+                        System.out.println("Cyfrą " + (losowaDoPodpowiedzi + 1) + "od lewej strony jest: " + tablica[losowaDoPodpowiedzi]);
+                        break;
+                        default:
+                            break;
+                    }
+
+                } else if (tablicaLiczba == odpowiedz) {
+                    System.out.println("Gratulacje! Podałeś prawidłową odpowiedź!");
+                    System.out.println("Czy chcesz grać dalej? Y - tak | N - nie");
+                    String coDalej = scanner.nextLine().toUpperCase();
+                    switch (coDalej) {
+                        case "Y":
+                            zycie = 6;
+                            break;
+                        case "N":
+                            System.exit(0);
+                            break;
+
+                    }
+                }
 
 
             } else {
                 System.out.println("Game over");
                 System.out.println();
-                System.out.println("Szyfrem była kombinacja: " + tablica.toString());
+                System.out.println("Szyfrem była kombinacja: " + tablicaLiczba);
                 System.out.println();
                 System.out.println("Czy chcesz grać dalej? Y - tak | N - nie");
                 String coDalej = scanner.nextLine().toUpperCase();
                 switch (coDalej) {
                     case "Y":
                         zycie = 6;
-
+                        break;
                     case "N":
                         System.exit(0);
                         break;
